51101653200332a8cc50378344e6e7a46a79fe19
Christian <Christian.Hansen9@outlook.com>
2019-10-24 15:34:44 -0500

    Add HelloWorld sln, project, and cs

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using sc = System.Console; // This means we are using the System library/assembly/namespace
+// and don't have to write 'System'
+
+namespace HelloWorld.Client
+{
+  /// <summary>
+  /// contains the start of the program
+  /// </summary>
+    internal class Program
+    {
+      /// <summary>
+      /// starts the program
+      /// </summary>
+      /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            sc.WriteLine("Hello World!");
+            sc.WriteLine(args);
+        }
+    }
+}
